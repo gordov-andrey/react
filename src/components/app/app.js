bbca15e2a56456a741bad96ece828fc3d728f96a
Andrey Gordov <gordov.ar@gmail.com>
2020-11-20 15:06:45 +0500

    rtytry

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import PostStatusFilter from '../post-status-filter';
@@ -13,25 +13,106 @@ const AppBlock = styled.div`
     max-width: 800px;
 `
 
-const App = () => {
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {label: 'Going to lern React', important: false, like: false, id: 'er'},
+                {label: 'That is so good', important: false, like: false, id: 'gt'},
+                {label: 'I need a break...', important: false, like: false, id: 'gb'}
+            ]
+        
+        };
+        this.deleteItem = this.deleteItem.bind(this);
+        this.addItem = this.addItem.bind(this);
+        this.onToggleImportant = this.onToggleImportant.bind(this);
+        this.onToggleLiked = this.onToggleLiked.bind(this);
 
-    const data = [
-        {label: 'Going to lern React', important: true, id: 'er'},
-        {label: 'That is so good', important: false, id: 'gt'},
-        {label: 'I need a break...', important: false, id: 'gb'}
-    ]
+        this.maxId = 4;
+    }
 
-    return (
-        <AppBlock>
-            <AppHeader/>
-            <div className='search-panel d-flex'>
-                <SearchPanel/>
-                <PostStatusFilter/>
-            </div>
-            <PostList posts={data}/>
-            <PostAddForm/>
-        </AppBlock>
-    )
+    deleteItem(id) {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id)
+            
+            const before = data.slice(0, index);
+            const after = data.slice(index + 1);
+
+            const newArr = [...before, ...after];
+
+            return {
+                data: newArr
+            }
+        });
+    }
+
+    addItem(text) {
+        const newItem = {
+            label: text,
+            important: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        })
+    }
+
+    onToggleImportant(id) {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+
+            const old = data[index];
+            const newItem = {...old, important: !old.important};
+
+            const newArr = [...data.slice(0, index), newItem, ...data.slice(index +1)];
+
+            return {
+                data: newArr
+            }
+        })
+    }
+
+    onToggleLiked(id) {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+
+            const old = data[index];
+            const newItem = {...old, like: !old.like};
+
+            const newArr = [...data.slice(0, index), newItem, ...data.slice(index +1)];
+
+            return {
+                data: newArr
+            }
+        })
+    }
+
+    render() {
+        const {data} = this.state;
+        const liked = data.filter(item => item.like).length;
+        const allPosts = data.length;
+
+        return (
+            <AppBlock>
+                <AppHeader
+                    liked={liked}
+                    allPosts={allPosts}/>
+                <div className='search-panel d-flex'>
+                    <SearchPanel/>
+                    <PostStatusFilter/>
+                </div>
+                <PostList 
+                    posts={this.state.data}
+                    onDelete={this.deleteItem}
+                    onToggleImportant={this.onToggleImportant}
+                    onToggleLiked={this.onToggleLiked}/>
+                <PostAddForm
+                    onAdd={this.addItem}/>
+            </AppBlock>
+        )
+    }
 }
-
-export default App;
--- src/components/post-add-form/post-add-form.js
+++ src/components/post-add-form/post-add-form.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './post-add-form.css';
 
-const PostAddForm = () => {
-    return (
-        <form className='bottom-panel d-flex'>
-            <input
-                type='text'
-                placeholder='О чем Вы думаете сейчас?'
-                className='form-control new-post-label'
-            ></input>
-            <button
-                type='submit'
-                className='btn btn-outline-secondary'
-            >Добавить</button>
-        </form>
-    )
+export default class PostAddForm extends Component {
+    constructor(props) {
+        super(props);
+        this.onValueChange = this.onValueChange.bind();
+    }
+
+    onValueChange(e) {
+        console.log(e.target.value);
+    }
+
+    render () {
+        return (
+            <form className='bottom-panel d-flex'>
+                <input
+                    type='text'
+                    placeholder='О чем Вы думаете сейчас?'
+                    className='form-control new-post-label'
+                    onChange={this.onValueChange}
+                ></input>
+                <button
+                    type='submit'
+                    className='btn btn-outline-secondary'
+                   >
+                    Добавить
+                </button>
+            </form>
+        )
+    }
 }
-export default PostAddForm;
